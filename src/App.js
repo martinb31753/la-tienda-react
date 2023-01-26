40d1e1d1a10fc46794b35eda7e2515ffe0032b52
martinb31753 <martinbotta85@gmail.com>
2023-01-26 14:25:56 -0300

    avanzando

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ItemListContainer from "./components/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import NavBar from "./components/NavBar";
 import CartContextProvaider from "./components/context/CartContext";
+import Cart from "./components/Cart";
 
 function App(){
 return(
@@ -16,6 +17,7 @@ return(
       <Route  path={"/"} element={ <ItemListContainer/>}/>
       <Route  path={"/category/:id"} element={ <ItemListContainer/>}/>
       <Route  path={"/item/:id"} element={ <ItemDetailContainer/>}/>
+      <Route  path={"/Cart"} element={ <Cart/>}/>
       <Route  path={"*"} element={ <Error404/>}/>
     </Routes>
    
--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { CartContext } from "./context/CartContext";
 
 const CartWidget = () => {
+    const{cartTotal}= useContext(CartContext);
     return(
-        <button type="button" className="btn btn-primary position-relative">
+        <Link to={"/Cart"} className="btn btn-primary position-relative">
     <img src={"./imagenes/cart-plus.svg"} alt={"carrito"} width={20} />
-  <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">1</span>
+  <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"> {cartTotal ()} 1</span>
    
   
-</button>
+</Link>
 
 
     )
